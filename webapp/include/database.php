a476e5494bacecfcb2fb033cbeed174d0e851bde
Lord Of Light <lordoflight@example.com>
2018-03-22 21:42:30 +0000

    Tweak sql_mode.

--- webapp/include/database.php
+++ webapp/include/database.php
@@ -30,7 +30,7 @@ function dbconn($mysql_host='', $mysql_user='', $mysql_pass='', $mysql_db='') {
     }
   }
   q('SET NAMES utf8','','',false);
-  q("SET SESSION sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'",'','',false);
+  q("SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'",'','',false);
   mysql_select_db($mysql_db) or die(sql_error_handler('dbconn: mysql_select_db', mysql_error(), mysql_errno()) );
 
   if (!isset($GLOBALS['db_links'])) $GLOBALS['db_links'] = array();
